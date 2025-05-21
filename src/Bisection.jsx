e4df3e26e7bf3794f3cc1db79aeeb97c112a28aa
Grace Hollenbach <llamaladycreates@gmail.com>
2025-05-21 08:36:39 -0400

    initial commit

---
+++ src/Bisection.jsx
@@ -0,0 +1,39 @@
+import './Bisection.css';
+import { InlineMath } from 'react-katex';
+import React from 'react';
+
+function Bisection() {
+  return (
+    <div className="bisection-container">
+      <section className="bisection-section">
+        <h1>Interval Halving (Bisection)</h1>
+        <ul>
+          <li>Interval halving (bisection), an ancient but effective method for finding a zero of <InlineMath math={'f(x)'}/></li>
+          <li>It begins with two values for <InlineMath math={"x"} /> that brackets a root.</li>
+          <li><b>The function <InlineMath math={'f(x)'}/> changes signs at these two x-values</b>, and if <InlineMath math={'f(x)'}/> is continuous, there must be at least one root between the values</li>
+          <li>The test to see that <InlineMath math={'f(x)'}/> does change sign between points <InlineMath math={'a'}/> and <InlineMath math={'b'}/> is to see if <InlineMath math={'f(a) \\cdot f(b) < 0'}/> (see Fig. 3.1)</li>
+        </ul>
+        <img src= {`${import.meta.env.BASE_URL}Fig 31.png`} alt="Figure 3.1" className="figure-img" />
+        <figcaption>Figure 3.1</figcaption>
+        <p>The bisection method then </p>
+        <ul>
+          <li>successively divides the initial interval in half,</li>
+          <li>finds in which half the root(s) must lie,</li>
+          <li>and repeats with the endpoints of the smaller interval.</li>
+        </ul>
+      </section>
+      <section className="bisection-section">
+        <h2>An algorithm for halving the interval </h2>
+        <img src={`${import.meta.env.BASE_URL}algorithm.png`} alt="algorithm" className="figure-img" style={{ width: 'auto', height: '200px' }}/>
+        <ul>
+          <li>Think about the multiplication factor, 2</li>
+          <li>The final value of <InlineMath math={'x_3'}/> approximates the root, and it is the error by not more than <InlineMath math={'\\frac{|x_1-x_2|}{2}'}/></li>
+          <li>The method may produce a false root if <InlineMath math ={"f(x)"}/> is discontinuous on <InlineMath math ={"[x_1, x_2]"}/></li>
+        </ul>
+
+      </section>
+    </div>
+  );
+}
+
+export default Bisection;
--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Bisection from './Bisection';
 import reportWebVitals from './reportWebVitals';
+import 'katex/dist/katex.min.css';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Bisection />
   </React.StrictMode>
 );
 
